81c44e32301fbe476bfc615ebc66f1de9b0b304a
orionvega2343-cloud <orionvega2343@gmail.com>
2026-07-11 22:56:23 +0300

    feat: Add models

---
+++ internal/models/area.go
@@ -0,0 +1,6 @@
+package models
+
+type Area struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
---
+++ internal/models/salary.go
@@ -0,0 +1,8 @@
+package models
+
+type Salary struct {
+	From     *int   `json:"from"`
+	To       *int   `json:"to"`
+	Currency string `json:"currency"`
+	Gross    bool   `json:"gross"`
+}
