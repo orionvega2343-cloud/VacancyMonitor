f68922976766a4d0d259290a0a43f91e12c7b052
orionvega2343-cloud <orionvega2343@gmail.com>
2026-07-12 16:26:43 +0300

    feat: Add MustLoad config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	PollInterval      int               `yaml:"poll_interval"`
+	WorkerCount       int               `yaml:"worker_count"`
+	ChannelBufferSize int               `yaml:"channel_buffer_size"`
+	ShutdownTimeout   int               `yaml:"shutdown_timeout"`
+	Filter            FilterHhConfig    `yaml:"filter"`
+	RateLimit         RateLimiterConfig `yaml:"rate_limit"`
+	Redis             RedisConfig       `yaml:"redis"`
+	BotToken          string            `env:"BOT_TOKEN" env-required:"true"`
+}
+
+type FilterHhConfig struct {
+	Keywords   []string `yaml:"keywords"`
+	Experience string   `yaml:"experience"`
+	Area       string   `yaml:"area"`
+	Remote     bool     `yaml:"remote"`
+}
+
+type RateLimiterConfig struct {
+	RequestsPerPeriod int `yaml:"requests_per_period"`
+	Period            int `yaml:"period"`
+}
+
+type RedisConfig struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+	DB   int    `yaml:"db"`
+}
+
+func MustLoad() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var cfg Config
+
+	err = cleanenv.ReadConfig("config/config.yml", &cfg)
+	if err != nil {
+		log.Fatal("Error loading config")
+	}
+	return &cfg
+
+}
