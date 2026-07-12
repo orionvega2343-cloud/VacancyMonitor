6eb6d2376996adf46d1d64f2d224700f113ff578
orionvega2343-cloud <orionvega2343@gmail.com>
2026-07-12 21:37:02 +0300

    feat: Add rate limiter

--- internal/api/rate_limit.go
+++ internal/api/rate_limit.go
@@ -1,1 +1,59 @@
 package api
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RateLimiter interface {
+	Wait(ctx context.Context) error
+}
+
+type RateLimiterImpl struct {
+	redisClient *redis.Client
+	key         string
+	limit       int
+	period      time.Duration
+}
+
+func NewRateLimiter(redisClient *redis.Client, key string, limit int, period time.Duration) *RateLimiterImpl {
+	return &RateLimiterImpl{redisClient: redisClient, key: key, limit: limit, period: period}
+}
+
+func (r *RateLimiterImpl) Wait(ctx context.Context) error {
+	for {
+
+		rdb := r.redisClient
+
+		val, err := rdb.Incr(ctx, r.key).Result()
+		if err != nil {
+			return err
+		}
+
+		if val == 1 {
+			_, err = rdb.Expire(ctx, r.key, r.period).Result()
+			if err != nil {
+				return err
+			}
+		}
+
+		if val > int64(r.limit) {
+			ttl, err := rdb.TTL(ctx, r.key).Result()
+			if err != nil {
+				return err
+			}
+			select {
+			case <-time.After(ttl):
+				// окно истекло, пробуем заново
+				continue
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+
+		return nil
+	}
+
+}
